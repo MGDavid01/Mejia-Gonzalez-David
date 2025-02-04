58ecbc8ada794b3e78e2f86e84ed0772672b6cb0
MGDavid01 <0322103760@ut-tijuana.edu.mx>
2025-02-04 12:34:57 -0800

    Delete AVANCE_FINAL directory

--- AVANCE_FINAL/LOGIXPRESS/includes/headUsers.php
+++
@@ -1,9 +0,0 @@
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu</title>
-    <link rel="stylesheet" href="css/menuBase.css">
-</head>
-<body>
-    <div class="container">
